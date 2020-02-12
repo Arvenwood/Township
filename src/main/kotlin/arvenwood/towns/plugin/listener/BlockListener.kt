8641b35ee6210bb21c5610ed44f322464940b2f8
xDotDash <xdotdash@gmail.com>
2020-02-11 22:36:07 -0600

    Protect sign changes

--- src/main/kotlin/arvenwood/towns/plugin/listener/BlockListener.kt
+++ src/main/kotlin/arvenwood/towns/plugin/listener/BlockListener.kt
@@ -9,6 +9,7 @@ import org.spongepowered.api.data.Transaction
 import org.spongepowered.api.entity.living.player.Player
 import org.spongepowered.api.event.Listener
 import org.spongepowered.api.event.block.ChangeBlockEvent
+import org.spongepowered.api.event.block.tileentity.ChangeSignEvent
 import org.spongepowered.api.event.filter.cause.Root
 import org.spongepowered.api.text.Text
 import org.spongepowered.api.text.format.TextColors.RED
@@ -34,11 +35,23 @@ class BlockListener {
             val location: Location<World> = transaction.original.location.orElse(null) ?: continue
             val claim: Claim = ClaimService.get().getClaimAt(location).orElse(null) ?: continue
 
-            if (resident.town != claim.town) {
+            if (claim.town != resident.town) {
                 event.isCancelled = true
-                player.sendMessage(Text.of(RED, "That chunk is owned by the town ${claim.town.name}"))
+                player.sendMessage(Text.of(RED, "That chunk is owned by the town ${claim.town.name}."))
                 return
             }
+        }
+    }
+
+    @Listener
+    fun onChangeSign(event: ChangeSignEvent, @Root player: Player) {
+        val resident: Resident = ResidentService.get().getResident(player.uniqueId).orElse(null) ?: return
+        val claim: Claim = ClaimService.get().getClaimAt(event.targetTile.location).orElse(null) ?: return
+
+        if (claim.town != resident.town) {
+            event.isCancelled = true
+            player.sendMessage(Text.of(RED, "That chunk is owned by the town ${claim.town.name}."))
+            return
         }
     }
 }
